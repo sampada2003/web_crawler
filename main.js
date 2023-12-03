2e853042d499645507ddd6038621f46422784cd3
unknown <sampadadeshpande2003@gmail.com>
2023-12-03 15:14:31 +0530

    printReport finished

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 process.argv
 
 const {crawlPage} = require('./crawl.js')
+const {printReport} = require('./report.js')
 async function main(){
     if(process.argv.length < 3){
         console.log("No website provided")
@@ -15,10 +16,7 @@ async function main(){
     const baseURL = process.argv[2]
     console.log(`Starting crawl of ${baseURL}`)
     const pages = await crawlPage(baseURL, baseURL, {})
-
-    for (const page of Object.entries(pages)){
-        console.log(page)
-    }
+    printReport(pages)
 }
 
 main()
